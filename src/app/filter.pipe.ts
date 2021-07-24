a93e82426cc6d9ff1431af8d71a16c6934d43ad1
akshaysawase <asawase4@gmail.com>
2021-07-24 14:00:54 +0530

    Search Filter Added

---
+++ src/app/filter.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+
+  transform(array: any[], text: string): any[] {
+    if(!array) return [];
+    if(!text) return array;
+    text = text.toLowerCase();
+    return array.filter((e1)=>{
+      return e1.toLowerCase().includes(text)
+    });
+  }
+
+}
